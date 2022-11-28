5289bc90c92c9a9cc2a543d169f861654553a335
Thibault <thibault.gillotin@hei.yncrea.fr>
2022-11-28 11:55:05 +0100

    bouton photos mariage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,19 @@
-import { Button, ButtonGroup, Container, Divider, Grid } from "@mui/material";
+import { Button, Card, CardContent, Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import React, { useEffect, useRef, useState } from "react";
-import { createSearchParams, useSearchParams } from "react-router-dom";
+import React from "react";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import Timer from "../components/Timer";
 import "./Home.css";
 
 function Home() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [hasReceivedInvitation, setHasReceivedInvitation] = useState(
-    searchParams.get("r") == null ? "1" : searchParams.get("r")
-  );
-  const isFirstMount = useRef(true);
-  const underBanner = useRef(null);
-
-  const setR = (value) => {
-    setHasReceivedInvitation(value);
-    setSearchParams(createSearchParams({ r: value }));
-  };
-
-  useEffect(() => {
-    if (isFirstMount.current) {
-      isFirstMount.current = false;
-    } else {
-      underBanner.current.scrollIntoView({ behavior: "smooth" });
-    }
-    return isFirstMount.current;
-  }, [hasReceivedInvitation]);
-
   return (
     <>
       <Header />
       <Box
         sx={{
           height: "100vh",
-          maxHeight: "130vw",
           background: "url(./img/lavande-photo.jpg) no-repeat",
           backgroundSize: "cover",
           backgroundPosition: "60% 30%",
@@ -52,7 +29,37 @@ function Home() {
             paddingBottom: 2,
           }}
         >
-          <Timer endDate="2022-08-20T13:30:00+02:00" />
+          <Card
+            variant="outlined"
+            sx={{
+              opacity: "0.83",
+              borderRadius: 2,
+              px: { xs: 0, sm: 3 },
+              mb: 2,
+              textAlign: "center",
+            }}
+          >
+            <CardContent>
+              <Box>
+                <Typography sx={{ fontWeight: "bold", fontSize: 30, mb: 3 }}>
+                  Mille mercis ! 🥰
+                </Typography>
+                <Button
+                  variant="contained"
+                  size="large"
+                  endIcon={<ChevronRightIcon />}
+                  onClick={() =>
+                    window.open(
+                      "https://drive.google.com/drive/folders/1g28900TSHbTw-fAJpqljl8aoFsFDeZg1",
+                      "_self"
+                    )
+                  }
+                >
+                  Découvir les photos
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
         </Grid>
       </Box>
       <Footer />
